8a69c6fd8687eaf211c1741e19f057501877a51e
andrew otis <andrew.otis@gmail.com>
2022-02-09 15:50:13 -0700

    updates

--- generateSystemKeys.js
+++ generateSystemKeys.js
@@ -2,18 +2,20 @@ const openpgp = require('openpgp');
 const fs = require('fs');
 
 (async () => {
+    const pass = "CHANGEME"
+
     const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
-        userIDs: [{ name: 'St Nicholas', email: 'saintnick@northpole.com' }], // you can pass multiple user IDs
-        passphrase: 'cZbT2TF]em@h4M&pEm+FqJ', // protects the private key
+        userIDs: [{ name: 'USER NAME', email: 'funky@buttloving.com' }], // you can pass multiple user IDs
+        passphrase: pass, // protects the private key
     });
 
 
 
-    const str = "REACT_APP_SYSTEM_PRIVATE_KEY=" + btoa(privateKey) + "\nREACT_APP_SYSTEM_PUBLIC_KEY" + btoa(publicKey)
-        + "\nREACT_APP_SYSTEM_KEY_PASSPHRASE=cZbT2TF]em@h4M&pEm+FqJ"
+    const str = "REACT_APP_SYSTEM_PRIVATE_KEY=" + btoa(privateKey) + "\nREACT_APP_SYSTEM_PUBLIC_KEY=" + btoa(publicKey)
+        + "\nREACT_APP_SYSTEM_KEY_PASSPHRASE=" + pass
     console.log(str)
 
-    fs.writeFile('system-keys.json', str, err => {
+    fs.writeFile('systemkeys', str, err => {
         if (err) {
             console.error(err)
             return
